d6fdf1469ee7149f86ef108a82bff8b6d6cbe4eb
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-29 21:37:01 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+Class Dog = ("dog")
+def bark 
+  puts "woof"
+end 
+Dog class #sit 
+puts "the dog is sitting"
+end 
+
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+Class Person 
+def = greeting
+  puts "Hello World!"
+end 
+
+Class Person 
+def = walks 
+  puts "the person is walking"
+end
